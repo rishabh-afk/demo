e85b06fbc5ea9541a820c98ae363880221f3ac55
Rishabh gupta <guptarishabh792@gmail.com>
2024-12-02 22:31:00 +0530

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true, // Disable image optimization for testing
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "encrypted-tbn2.gstatic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "customiseitnow.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "www.clothes2order.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.worksafeonline.co.uk",
+      },
+      {
+        protocol: "https",
+        hostname: "demo2.wpopal.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
